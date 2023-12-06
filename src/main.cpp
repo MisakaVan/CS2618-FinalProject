1d0f1ba4f948da72b313cc448d302c5742a2e5f5
MisakaVan <2102315149@qq.com>
2023-12-07 01:29:51 +0800

    Added: a function to test all basic functionality except for the drawing part

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,17 @@
 #include <Arduino.h>
 #include "resistor_measure.h"
 #include "capacitor_measure.h"
+#include "transistor_measure.h"
+#include "detect_condition.h"
+
+/**
+ * Basic function test for measuring the R, C, T. Logs to Serial.
+ */
+void testMainMeasurementToSerial(int intervalMs = 0);
+
 
 void setup()
 {
-// write your initialization code here
     Serial.begin(9600);
     while (!Serial) {
         delay(10);
@@ -24,9 +31,57 @@ void setup()
 
 void loop()
 {
-    // test resistance measure
-    testResistanceMeasure(PORTS[0], PORTS[1]);
+//    // test resistance measure
+//    testResistanceMeasure(PORTS[0], PORTS[1]);
+//
+//    delay(1000);
 
-    delay(1000);
+    testMainMeasurementToSerial(5000);
 }
 
+
+void testMainMeasurementToSerial(int intervalMs)
+{
+    ConnectionState curState{};
+    detectConnection(curState);
+    switch (curState.mode) {
+        case ConnectionMode::Null: {
+            printMsg("No connection detected");
+        }
+            break;
+        case ConnectionMode::R: {
+            printMsg("Resistance: %d",
+                     static_cast<int>(measureResistance<RMeasureMode::LowR>(
+                             PORTS[curState.data.r.port1],
+                             PORTS[curState.data.r.port2]
+                     )));
+            break;
+        }
+        case ConnectionMode::C: {
+            printMsg("Capacitance: %d",
+                     static_cast<int>(measureCapacitance<CMeasureMode::LowR>(
+                             PORTS[curState.data.c.port1],
+                             PORTS[curState.data.c.port2]
+                     )));
+            break;
+        }
+        case ConnectionMode::T: {
+            auto transistorType = curState.data.t.type;
+            auto measureFunc = transistorType == TransistorType::PNP ?
+                               measureTransistorBeta<TransistorType::PNP> : measureTransistorBeta<TransistorType::NPN>;
+
+            printMsg("Transistor Type: %s",
+                     transistorType == TransistorType::PNP ? "PNP" : "NPN");
+            printMsg("Transistor Beta: %d",
+                     static_cast<int>(measureFunc(
+                             PORTS[curState.data.t.portB],
+                             PORTS[curState.data.t.portC],
+                             PORTS[curState.data.t.portE]
+                     )));
+            break;
+        }
+    }
+
+    delay(intervalMs);
+
+}
