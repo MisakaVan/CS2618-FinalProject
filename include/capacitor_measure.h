56321793783e185c9d9529e82554f0c0d39dc0d9
MisakaVan <2102315149@qq.com>
2023-12-17 22:01:59 +0800

    commented an unused func.

--- include/capacitor_measure.h
+++ include/capacitor_measure.h
@@ -21,45 +21,45 @@ void dischargeCapacitor(const Port &port1, const Port &port2)
 }
 
 
-/**
- * Measure the capacitance of the capacitor connected to the given two ports.
- * By measuring the time it takes to charge the capacitor to 63.2% of VCC.
- * @param port1
- * @param port2
- * @return
- */
-template <CMeasureMode mode>
-capacitance_nanoF_t measureCapacitance(const Port &port1, const Port &port2)
-{
-    // VCC(port1.analogPin=HIGH) -> C to be measured --*-> ConstantR -> GND(port2.digitalPinHighR=LOW)
-    //                                                 *-> AnalogPin(port2.analogPin)
-    // use port2.analogPin to measure the voltageOnConstantR between R and port2.digitalPinHighR (*)
-
-
-    clearPorts();
-    pinMode(port1.analogPin, OUTPUT);
-    digitalWrite(port1.analogPin, HIGH);
-    auto digitalPin = mode == CMeasureMode::LowR ? port2.digitalPinLowR : port2.digitalPinHighR;
-    pinMode(digitalPin, OUTPUT);
-
-    auto startTime = millis();
-    digitalWrite(digitalPin, HIGH);
-    while (getVoltageAtAnalogPin(port2) > VCC * 0.368) {
-        delay(1);
-    }
-    auto endTime = millis();
-    digitalWrite(digitalPin, LOW);
-
-    auto timeMiliSec = endTime - startTime;
-    auto R = mode == CMeasureMode::LowR ? LOW_RESISTANCE : HIGH_RESISTANCE;
-
-    // time = R * C
-    // 1000 ms = 1 ohm * 1e9 nF
-    auto capacitance = static_cast<capacitance_nanoF_t>((timeMiliSec / R) * 1000000ll );
-    return capacitance;
-
-    // TODO: remains to be tested!
-}
+///**
+// * Measure the capacitance of the capacitor connected to the given two ports.
+// * By measuring the time it takes to charge the capacitor to 63.2% of VCC.
+// * @param port1
+// * @param port2
+// * @return
+// */
+//template <CMeasureMode mode>
+//capacitance_nanoF_t measureCapacitance(const Port &port1, const Port &port2)
+//{
+//    // VCC(port1.analogPin=HIGH) -> C to be measured --*-> ConstantR -> GND(port2.digitalPinHighR=LOW)
+//    //                                                 *-> AnalogPin(port2.analogPin)
+//    // use port2.analogPin to measure the voltageOnConstantR between R and port2.digitalPinHighR (*)
+//
+//
+//    clearPorts();
+//    pinMode(port1.analogPin, OUTPUT);
+//    digitalWrite(port1.analogPin, HIGH);
+//    auto digitalPin = mode == CMeasureMode::LowR ? port2.digitalPinLowR : port2.digitalPinHighR;
+//    pinMode(digitalPin, OUTPUT);
+//
+//    auto startTime = millis();
+//    digitalWrite(digitalPin, HIGH);
+//    while (getVoltageAtAnalogPin(port2) > VCC * 0.368) {
+//        delay(1);
+//    }
+//    auto endTime = millis();
+//    digitalWrite(digitalPin, LOW);
+//
+//    auto timeMiliSec = endTime - startTime;
+//    auto R = mode == CMeasureMode::LowR ? LOW_RESISTANCE : HIGH_RESISTANCE;
+//
+//    // time = R * C
+//    // 1000 ms = 1 ohm * 1e9 nF
+//    auto capacitance = static_cast<capacitance_nanoF_t>((timeMiliSec / R) * 1000000ll );
+//    return capacitance;
+//
+//    // TODO: remains to be tested!
+//}
 
 //void capacitorMeasurePrototype()
 //{
